87b4990d87b3218f5396b06de682d27bf26899ca
Ted <kimtx2017@gmail.com>
2017-06-12 21:41:58 +0900

    # Fix bug
    - Correct Logic 'Insert FIRST NODE into linked list'

--- LinkedList/LinkedList/LinkedList/main.cpp
+++ LinkedList/LinkedList/LinkedList/main.cpp
@@ -8,8 +8,142 @@
 
 #include <iostream>
 
+using namespace std;
+
+class Node{
+    
+public:
+    
+    int data;
+    Node* next;
+    
+    Node(int num){
+        data = num;
+        next = NULL;
+    };
+    
+    ~Node(){};
+    
+};
+
+class LinkedList{
+    
+public:
+    
+    Node *head;
+    Node *tail;
+    
+    LinkedList(){
+        head = NULL;
+        tail = NULL;
+    }
+    
+    void insertNode(Node *newNode){
+        
+        if(head == NULL)
+            head = newNode;
+        else
+            tail->next = newNode;
+        
+        tail = newNode;
+        
+    };
+    
+    void insertNode(Node *newNode, int position){
+        
+        Node *front = findNode(position);
+        Node *back = findNode(position-1);
+        
+        newNode->next = front;
+        back->next = newNode;
+        
+    }
+    
+    void deleteNode(){
+        
+        Node *pointer = head;
+        
+        while(pointer->next != tail)
+            pointer = pointer->next;
+        
+        cout << tail->data << " is deleted !!" << endl;
+        tail->~Node();
+        
+        pointer->next = NULL;
+        tail = pointer;
+        
+        cout << "Now, tail is " << tail->data << endl;
+        
+    }
+    
+    void deleteNode(int position){
+        
+        Node *front = findNode(position+1);
+        Node *target = findNode(position);
+        Node *back = findNode(position-1);
+        
+        back->next = front;
+        target->next = NULL;
+        target->~Node();
+        
+    };
+    
+    Node* findNode(int position){
+        
+        Node *pointer = head;
+        
+        for(int i=0; i<position-1; i++){
+            pointer = pointer->next;
+        }
+        
+        return pointer;
+    };
+    
+    int listCount(){
+        
+        Node *pointer = head;
+        int count=0;
+        
+        while(pointer != NULL)
+            count++;
+        
+        return count;
+    };
+    
+    void showList(){
+        cout << "----------------------------" << endl;
+        
+        Node *pointer = head;
+        
+        while(pointer != NULL){
+            cout << pointer->data << " ";
+            pointer = pointer->next;
+        }
+        
+        cout << endl << "----------------------------" << endl;
+    }
+};
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    LinkedList list = LinkedList();
+    
+    for(int i=0; i<10; i++){
+        
+        Node *newNode = new Node(i);
+        list.insertNode(newNode);
+        
+    }
+    
+    list.showList();
+    
+    list.deleteNode();
+    list.showList();
+    
+    Node *newNode = new Node(71);
+    list.insertNode(newNode, 8);
+    list.showList();
+    
     return 0;
 }
+
