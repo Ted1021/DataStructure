e7351de65758c6bd7b3b73ed229dc9bd7703c931
Ted <kimtx2017@gmail.com>
2017-06-12 22:53:12 +0900

    # Define Stack ADT

---
+++ LinkedList/Stack/Stack/Stack/main.cpp
@@ -0,0 +1,48 @@
+//
+//  main.cpp
+//  Stack
+//
+//  Created by Ted on 2017. 6. 12..
+//  Copyright © 2017년 Ted. All rights reserved.
+//
+
+// init, push, pop, peek?
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class MyStack{
+    
+public:
+    
+    MyStack(){
+        
+    };
+    
+    ~MyStack(){
+        cout << "Stack is deleted" << endl;
+    };
+    
+    void s_push(int num){
+        
+    };
+    
+    void s_pop(){
+        
+    };
+    
+    int s_peek(int target){
+        
+        
+        return 0;
+    };
+    
+    
+};
+
+int main(int argc, const char * argv[]) {
+   
+    return 0;
+}
